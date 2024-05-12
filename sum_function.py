207433bc78eeed7efb349ae20411233226eac4b5
Winsor <winsoromar12@gmail.com>
2024-05-12 13:29:58 -0400

    refactor: change variable x to res

--- sum_function.py
+++ sum_function.py
@@ -5,9 +5,9 @@ def sumThreeNumbers(num1, num2, num3):
     return num1 + num2 + num3
 
 def sumManyNumbers(a):
-    x = 0
+    res = 0
     for n in a:
-        x = x + n
-    return x
+        res = res + n
+    return res
 
 print(sumManyNumbers([1, 2, 3, 4]))
