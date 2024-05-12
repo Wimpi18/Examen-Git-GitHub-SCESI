4719ccd35bdc434e39a255de73dff1c14f729df8
Winsor <winsoromar12@gmail.com>
2024-05-12 13:31:47 -0400

    fix: delete sum three numbers function

--- sum_function.py
+++ sum_function.py
@@ -1,13 +1,8 @@
 def sumTwoNumbers(num1, num2):
     return num1 + num2
 
-def sumThreeNumbers(num1, num2, num3):
-    return num1 + num2 + num3
-
-def sumManyNumbers(a):
+def sumManyNumbers(numbersArray):
     res = 0
-    for number in a:
+    for number in numbersArray:
         res = res + number
     return res
-
-print(sumManyNumbers([1, 2, 3, 4]))
