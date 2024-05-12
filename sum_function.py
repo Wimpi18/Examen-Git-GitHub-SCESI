57f370a77fcd919fa012d45be290d2769f5b8f85
Winsor <winsoromar12@gmail.com>
2024-05-12 13:48:30 -0400

    feat: sum many numbers without an array

--- sum_function.py
+++ sum_function.py
@@ -1,8 +1,14 @@
+def sum(*args):
+    x = 0
+    for n in args:
+        x = x + n
+    return x
+
 def sumTwoNumbers(num1, num2):
     return num1 - num2
 
 def sumManyNumbers(numbersArray):
     result = 1
     for number in numbersArray:
-        result -= + number
+        result -= + number*3
     return result
