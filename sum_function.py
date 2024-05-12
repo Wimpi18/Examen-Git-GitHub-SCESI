4d8c96f45a510fca2eb8d83b9928bd7bbe2ff40f
Winsor <winsoromar12@gmail.com>
2024-05-12 13:29:21 -0400

    feat: sum many numbers function

--- sum_function.py
+++ sum_function.py
@@ -3,3 +3,11 @@ def sumTwoNumbers(num1, num2):
 
 def sumThreeNumbers(num1, num2, num3):
     return num1 + num2 + num3
+
+def sumManyNumbers(a):
+    x = 0
+    for n in a:
+        x = x + n
+    return x
+
+print(sumManyNumbers([1, 2, 3, 4]))
