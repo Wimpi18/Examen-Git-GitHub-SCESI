d4f9b5fb3434f716d8705607efe7da02bde7103d
Winsor <winsoromar12@gmail.com>
2024-05-12 19:24:52 -0400

    fix: last function multiply two numbers

--- multiply_function.py
+++ multiply_function.py
@@ -4,11 +4,11 @@ def multiply(*numbers):
         result *= number
     return result
 
-def multiplyTwoNumbers(num1, num2):
-    return num1 * num2
-
 def multiplyManyNumbers(numbersArray):
     result = 1
     for number in numbersArray:
         result *= number
     return result
+
+def multiplyTwoNumbers(num1, num2):
+    return num1 * num2
